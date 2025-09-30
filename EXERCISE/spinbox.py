0b573649c9e15e2798d7beb2a4e32e3e60c42eeb
RAYHAN ALI <515081@my.cwc.ac.uk>
2025-09-30 11:41:40 +0100

    spinbox exercise 2

--- EXERCISE/spinbox.py
+++ EXERCISE/spinbox.py
@@ -1,10 +1,29 @@
 import tkinter as tk
 from tkinter import ttk
 
+def update_label(*args):
+    value = value_var.get()
+    display_label.config(text=f"Selected Value: {value}")
+
 #This section creates the Tkinter window and adds the required elemnts to it
 window = tk.Tk()
 window.title("Tkinter Spinbox Widget")
 window.geometry("400x400")
+
+value_var = tk.DoubleVar()
+value_var.trace_add("write", update_label)  
+
+number_spinbox = ttk.Spinbox(
+    window,
+    from_=0,
+    to=100,
+    increment=0.01,
+    textvariable=value_var,
+    font='Calibri 24 bold'
+)
+number_spinbox.pack(pady=20)
+
+Number_spinbox = ttk.spinbox('Maths','English','Science','Computing','Latin','Arabic','Social Studies','Sports Science')
 
 #This is our spinbox
 int_var = tk.IntVar()
@@ -15,4 +34,3 @@ Number_Spinbox.pack
 
 #run the program to generate window with all packed elements for user interaction
 window.mainloop()
-
