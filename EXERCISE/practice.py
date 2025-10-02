2783d44bb45b775ef739d743a851879f0675fad9
515081 <515081@PC1NE8T14043>
2025-10-02 14:25:42 +0100

    simple grading system

--- EXERCISE/practice.py
+++ EXERCISE/practice.py
@@ -2,11 +2,17 @@ import tkinter as tk
 from tkinter import ttk
 
 window = tk.Tk()
-window.title("Tkinter Label Widget")
-window.geometry("500x500")
+window.title("Simple grading system")
+window.geometry("800x700")
 
-output_label = ttk.Label(master = window, text="Rayhan is Da Goat!",font='Calibri 24 bold',foreground= 'green', underline = 4)
+output_label = ttk.Title(master = window, text="Enter Student Grade:",font='Calibri 32 bold', borderwidth=1, relief='flat')
+
+
+string_var = tk.StringVar()
+Text_entry = ttk.Entry(master=window, textvariable=string_var, font='Calibri 24 bold')
+
 
 output_label.pack()
+Text_entry.pack(pady=20)
 
 window.mainloop()
